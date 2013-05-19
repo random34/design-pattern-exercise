0741be11ac68fea2498e69a3dab29088984d8459
random34 <laiqinan@gmail.com>
2013-05-19 12:08:53 +0100

    Comment: abstract factory is reconiseable by...

--- design-pattern-ex/src/designPattern/abstractFactory/AbstractFactoryTest.java
+++ design-pattern-ex/src/designPattern/abstractFactory/AbstractFactoryTest.java
@@ -1,9 +1,11 @@
 package designPattern.abstractFactory;
 
 /**
- * The <b>abstract factory</b> pattern is a software creational design pattern that
+ * <p>The <b>abstract factory</b> pattern is a software creational design pattern that
  * provides a way to encapsulate a group of individual factories that have a
- * common theme without specifying their concrete classes.
+ * common theme without specifying their concrete classes.</p>
+ * <p>recognizeable by creational methods returning the factory itself which in
+ * turn can be used to create another abstract/interface type</p>
  * @author laiq
  *
  */
